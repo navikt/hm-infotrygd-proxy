625da450be941fe56da7ea0cde4e3230cfd5562c
Kristoffer A. Iversen <kristoffer.andreas.iversen@nav.no>
2021-03-15 13:22:22 +0100

    Added more targeted test code for masked test db, prepared stmt.

--- src/main/kotlin/no/nav/hjelpemidler/Application.kt
+++ src/main/kotlin/no/nav/hjelpemidler/Application.kt
@@ -4,10 +4,13 @@ import mu.KotlinLogging
 import no.nav.hjelpemidler.configuration.Configuration
 import oracle.jdbc.OracleConnection
 import oracle.jdbc.pool.OracleDataSource
+import java.sql.PreparedStatement
 import java.util.Properties
 
 private val logg = KotlinLogging.logger {}
 // private val sikkerlogg = KotlinLogging.logger("tjenestekall")
+
+private var prStmtDecisionResult: PreparedStatement? = null
 
 fun main() {
     logg.info("Hello world")
@@ -22,40 +25,43 @@ fun main() {
     ods.url = Configuration.oracleDatabaseConfig["HM_INFOTRYGD_PROXY_DB_URL"]!!
     ods.connectionProperties = info
 
-    try {
-        logg.info("Connecting to database")
-        val connection = ods.getConnection()
+    logg.info("Connecting to database")
+    val connection = ods.getConnection()
 
-        logg.info("Fetching db metadata")
-        val dbmd = connection.metaData
-        println("Driver Name: " + dbmd.driverName)
-        println("Driver Version: " + dbmd.driverVersion)
+    logg.info("Fetching db metadata")
+    val dbmd = connection.metaData
+    logg.info("Driver Name: " + dbmd.driverName)
+    logg.info("Driver Version: " + dbmd.driverVersion)
 
-        println("")
-        println("Tables we have access to:")
-        connection.createStatement().use { statement ->
-            statement.executeQuery("SELECT owner, table_name FROM all_tables").use { resultSet ->
-                while (resultSet.next()) {
-                    println(resultSet.getString(1) + " " + resultSet.getString(2) + " ")
-                }
-            }
-        }
+    prStmtDecisionResult = connection.prepareStatement("SELECT S10_RESULTAT as resultat FROM INFOTRYGD_HJQ.SA_SAK_10 WHERE S01_PERSONKEY = ? AND S05_SAKSBLOKK = ? AND S10_SAKSNR = ?")
 
-        println("")
-        println("All tables:")
-        connection.createStatement().use { statement ->
-            statement.executeQuery("SELECT table_name FROM dba_tables").use { resultSet ->
-                while (resultSet.next()) {
-                    println(resultSet.getString(1) + " ")
-                }
-            }
-        }
+    val testFakeTkNr = "2103"
+    val testFakeFNR = "07010589518"
+    val testFakeSaksBlokk = "A"
+    val testFakeSaksNr = "04"
 
-    } catch (e: Exception) {
-        println("Exception: " + e.message.toString())
-        e.printStackTrace()
-    }
+    val result = queryForDecisionResult(testFakeTkNr, testFakeFNR, testFakeSaksBlokk, testFakeSaksNr)
+    logg.info("Result was: \"$result\" (should be \"IM\")")
+
+    prStmtDecisionResult!!.close()
 
     logg.info("Processing done, sleeping forever.")
     Thread.sleep(1000*60*60*24)
+}
+
+fun queryForDecisionResult(tkNr: String, fnr: String, saksblokk: String, saksnr: String): String {
+    val stmt = prStmtDecisionResult!!
+
+    stmt.clearParameters()
+    stmt.setString(1, "$tkNr$fnr") // S01_PERSONKEY
+    stmt.setString(2, saksblokk) // S05_SAKSBLOKK
+    stmt.setString(3, saksnr) // S10_SAKSNR
+
+    stmt.executeQuery().use { rs ->
+        while (rs.next()) {
+            return rs.getString("resultat")
+        }
+    }
+
+    throw Exception("no such db row")
 }
