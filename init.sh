530eeedb5981e874f3425dd7fdb02ed9de199c8f
Kristoffer A. Iversen <kristoffer.andreas.iversen@nav.no>
2021-03-15 12:45:14 +0100

    Fixed init.sh-script so that it loads the vault credentials/config for the oracle database correctly

--- init.sh
+++ init.sh
@@ -1,2 +1,3 @@
-#export HM_INFOTRYGD_PROXY_DB_USER=$(cat /secret/serviceuser/username)
-#export HM_INFOTRYGD_PROXY_DB_PWD=$(cat /secret/serviceuser/password)
+export HM_INFOTRYGD_PROXY_DB_URL=$(cat /secrets/infotrygd_hj/config/jdbc_url)
+export HM_INFOTRYGD_PROXY_DB_USR=$(cat /secrets/infotrygd_hj/credentials/username)
+export HM_INFOTRYGD_PROXY_DB_PW=$(cat /secrets/infotrygd_hj/credentials/password)
