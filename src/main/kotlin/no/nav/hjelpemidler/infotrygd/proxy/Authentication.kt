adb899278bfd3a4495734d4feab8e5bfe44b54c3
Erik Eiesland <erik.eiesland@nav.no>
2024-06-20 10:25:57 +0200

    hm-katalog -> 0.2.21 hvor EntraID blir EntraIDEnvironmentVariable

--- src/main/kotlin/no/nav/hjelpemidler/infotrygd/proxy/Authentication.kt
+++ src/main/kotlin/no/nav/hjelpemidler/infotrygd/proxy/Authentication.kt
@@ -6,20 +6,20 @@ import io.ktor.server.application.install
 import io.ktor.server.auth.Authentication
 import io.ktor.server.auth.jwt.JWTPrincipal
 import io.ktor.server.auth.jwt.jwt
-import no.nav.hjelpemidler.configuration.EntraID
+import no.nav.hjelpemidler.configuration.EntraIDEnvironmentVariable
 import java.net.URI
 import java.util.concurrent.TimeUnit
 
 fun Application.installAuthentication() {
-    val jwkProvider = JwkProviderBuilder(URI(EntraID.AZURE_OPENID_CONFIG_JWKS_URI).toURL())
+    val jwkProvider = JwkProviderBuilder(URI(EntraIDEnvironmentVariable.AZURE_OPENID_CONFIG_JWKS_URI).toURL())
         .cached(10, 24, TimeUnit.HOURS)
         .rateLimited(10, 1, TimeUnit.MINUTES)
         .build()
 
     install(Authentication) {
         jwt("aad") {
-            verifier(jwkProvider, EntraID.AZURE_OPENID_CONFIG_ISSUER) {
-                withAudience(EntraID.AZURE_APP_CLIENT_ID)
+            verifier(jwkProvider, EntraIDEnvironmentVariable.AZURE_OPENID_CONFIG_ISSUER) {
+                withAudience(EntraIDEnvironmentVariable.AZURE_APP_CLIENT_ID)
             }
             validate { credentials -> JWTPrincipal(credentials.payload) }
         }
