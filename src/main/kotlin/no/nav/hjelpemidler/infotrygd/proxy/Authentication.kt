7c74fbbbcd1526b02175bd656d0d783c8cf8a981
Erik Eiesland <erik.eiesland@nav.no>
2024-06-20 10:09:31 +0200

    Entra ID-miljøvariabler fra hm-core

--- src/main/kotlin/no/nav/hjelpemidler/infotrygd/proxy/Authentication.kt
+++ src/main/kotlin/no/nav/hjelpemidler/infotrygd/proxy/Authentication.kt
@@ -6,19 +6,20 @@ import io.ktor.server.application.install
 import io.ktor.server.auth.Authentication
 import io.ktor.server.auth.jwt.JWTPrincipal
 import io.ktor.server.auth.jwt.jwt
+import no.nav.hjelpemidler.configuration.EntraID
 import java.net.URI
 import java.util.concurrent.TimeUnit
 
 fun Application.installAuthentication() {
-    val jwkProvider = JwkProviderBuilder(URI(Configuration.AZURE_OPENID_CONFIG_JWKS_URI).toURL())
+    val jwkProvider = JwkProviderBuilder(URI(EntraID.AZURE_OPENID_CONFIG_JWKS_URI).toURL())
         .cached(10, 24, TimeUnit.HOURS)
         .rateLimited(10, 1, TimeUnit.MINUTES)
         .build()
 
     install(Authentication) {
         jwt("aad") {
-            verifier(jwkProvider, Configuration.AZURE_OPENID_CONFIG_ISSUER) {
-                withAudience(Configuration.AZURE_APP_CLIENT_ID)
+            verifier(jwkProvider, EntraID.AZURE_OPENID_CONFIG_ISSUER) {
+                withAudience(EntraID.AZURE_APP_CLIENT_ID)
             }
             validate { credentials -> JWTPrincipal(credentials.payload) }
         }
--- src/main/kotlin/no/nav/hjelpemidler/infotrygd/proxy/Configuration.kt
+++ src/main/kotlin/no/nav/hjelpemidler/infotrygd/proxy/Configuration.kt
@@ -1,7 +1,6 @@
 package no.nav.hjelpemidler.infotrygd.proxy
 
 import no.nav.hjelpemidler.configuration.EnvironmentVariable
-import no.nav.hjelpemidler.configuration.External
 import no.nav.hjelpemidler.configuration.vaultSecret
 
 object Configuration {
@@ -10,14 +9,4 @@ object Configuration {
     val HM_INFOTRYGD_PROXY_DB_USERNAME by vaultSecret("/secrets/infotrygd_hj/credentials/username")
     val HM_INFOTRYGD_PROXY_DB_PASSWORD by vaultSecret("/secrets/infotrygd_hj/credentials/password")
     val HM_INFOTRYGD_PROXY_DB_NAME by EnvironmentVariable
-
-    // Entra ID
-    @External
-    val AZURE_APP_CLIENT_ID by EnvironmentVariable
-
-    @External
-    val AZURE_OPENID_CONFIG_ISSUER by EnvironmentVariable
-
-    @External
-    val AZURE_OPENID_CONFIG_JWKS_URI by EnvironmentVariable
 }
