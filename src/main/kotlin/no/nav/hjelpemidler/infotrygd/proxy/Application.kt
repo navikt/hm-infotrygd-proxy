d967a266de4510a5657f4b2295c816cd98f3ed6a
Kristoffer A. Iversen <kristoffer.andreas.iversen@nav.no>
2024-11-13 09:07:58 +0100

    Revert "Debugging version"
    This reverts commit 645db89f2d107fd696753e9d5b2851885da75468.

--- src/main/kotlin/no/nav/hjelpemidler/infotrygd/proxy/Application.kt
+++ src/main/kotlin/no/nav/hjelpemidler/infotrygd/proxy/Application.kt
@@ -6,14 +6,22 @@ import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import io.github.oshai.kotlinlogging.KotlinLogging
 import io.ktor.serialization.jackson.jackson
 import io.ktor.server.application.Application
+import io.ktor.server.application.ApplicationStopping
 import io.ktor.server.application.call
 import io.ktor.server.application.install
+import io.ktor.server.auth.authenticate
 import io.ktor.server.metrics.micrometer.MicrometerMetrics
 import io.ktor.server.netty.EngineMain
 import io.ktor.server.plugins.callloging.CallLogging
 import io.ktor.server.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.server.request.path
+import io.ktor.server.request.receive
+import io.ktor.server.response.respond
+import io.ktor.server.routing.post
 import io.ktor.server.routing.routing
+import no.nav.hjelpemidler.configuration.Environment
+import no.nav.hjelpemidler.database.Oracle
+import no.nav.hjelpemidler.database.createDataSource
 import org.slf4j.event.Level
 
 private val log = KotlinLogging.logger {}
@@ -44,7 +52,7 @@ fun Application.module() {
 
     install(MicrometerMetrics) { registry = Prometheus.registry }
 
-    /* val database = createDataSource(Oracle) {
+    val database = createDataSource(Oracle) {
         jdbcUrl = Configuration.HM_INFOTRYGD_PROXY_DB_JDBC_URL
         username = Configuration.HM_INFOTRYGD_PROXY_DB_USERNAME
         password = Configuration.HM_INFOTRYGD_PROXY_DB_PASSWORD
@@ -88,16 +96,6 @@ fun Application.module() {
                 val response = infotrygdService.hentSakerForBruker(request.fnr)
                 call.respond(response)
             }
-        }
-    }
-     */
-    routing {
-        get("/isalive") {
-            call.respondText("READY")
-        }
-
-        get("/isready") {
-            call.respondText("READY")
         }
     }
 }
