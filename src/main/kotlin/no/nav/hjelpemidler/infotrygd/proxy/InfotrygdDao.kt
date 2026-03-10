72a6ccc2176eaa84a45ca9aa6566649c1897c0e0
Christian Bjørnsrud <christian.kristensen.bjornsrud@nav.no>
2026-03-10 14:18:42 +0100

    Oppdater og formater query.

--- src/main/kotlin/no/nav/hjelpemidler/infotrygd/proxy/InfotrygdDao.kt
+++ src/main/kotlin/no/nav/hjelpemidler/infotrygd/proxy/InfotrygdDao.kt
@@ -145,30 +145,28 @@ class InfotrygdDao(private val tx: JdbcOperations) {
         ).tilInfotrygdformat(),
     ) { true } == true
 
-    fun harVedtakOmHøreapparat(fnr: Fødselsnummer): HarVedtakOmHøreapparatResponse =
-        tx.singleOrNull(
-            """
-            SELECT TO_DATE(LPAD(S10_VEDTAKSDATO, 8, '0') DEFAULT '01011900' ON CONVERSION ERROR, 'DDMMYYYY') AS S10_VEDTAKSDATO
-            FROM SA_SAK_10
-            WHERE F_NR = :fnr
-              AND S10_VALG = 'HØ'
-              AND (S10_UNDERVALG = 'DA' OR S10_UNDERVALG = 'UL') -- DA og UL er begge Dagliglivet
-              AND (DB_SPLITT = 'HJ' OR DB_SPLITT = '99')
-            ORDER BY S10_VEDTAKSDATO DESC
-            FETCH FIRST 1 ROWS ONLY
-            """.trimIndent(),
-            mapOf(
-                "fnr" to fnr,
-            ).tilInfotrygdformat(),
-        ) { row ->
-            HarVedtakOmHøreapparatResponse(
-                vedtaksdato = row.localDateOrNull("S10_VEDTAKSDATO"),
-                harVedtak = true,
-            )
-        } ?: HarVedtakOmHøreapparatResponse(
-            vedtaksdato = null,
-            harVedtak = false,
+    fun harVedtakOmHøreapparat(fnr: Fødselsnummer): HarVedtakOmHøreapparatResponse = tx.singleOrNull(
+        """
+             SELECT S10_VEDTAKSDATO
+             FROM SA_SAK_10
+             WHERE F_NR = :fnr
+                 AND S10_VALG = 'HØ'
+                 AND (S10_UNDERVALG = 'DA' OR S10_UNDERVALG = 'UL')
+             ORDER BY TO_DATE(LPAD(S10_VEDTAKSDATO,8, '0') DEFAULT '01011900' ON CONVERSION ERROR,'DDMMYYYY') DESC
+             FETCH FIRST 1 ROWS ONLY
+        """.trimIndent(),
+        mapOf(
+            "fnr" to fnr,
+        ).tilInfotrygdformat(),
+    ) { row ->
+        HarVedtakOmHøreapparatResponse(
+            vedtaksdato = row.infotrygdDateOrNull("S10_VEDTAKSDATO"),
+            harVedtak = true,
         )
+    } ?: HarVedtakOmHøreapparatResponse(
+        vedtaksdato = null,
+        harVedtak = false,
+    )
 
     fun harVedtakFraFør(fnr: Fødselsnummer): Boolean = tx.singleOrNull(
         """
